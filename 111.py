3c1304df742a530eae4c5ed38732a767dde3776d
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-14 22:56:44 +0530

    Update 111.py

--- 111.py
+++ 111.py
@@ -3,7 +3,7 @@ ax=list(map(int,input().split()))
 c=[]
 for i in range(0,a):
     for j in range(a,len(ax)):
-        if ax[i]==ax[j]:
+        if ax[i]==ax[j] and ax[i] not in range(i+1,a):
             c.append(ax[i])
 c.sort()
 print(*c)
